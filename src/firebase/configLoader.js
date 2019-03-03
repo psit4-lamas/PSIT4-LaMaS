4af12540f9af46dea33ffa28f1937e6954938003
tduong992 <tduong992@gmail.com>
2019-03-03 13:23:18 +0100

    Add config loader for dev and prod environments

---
+++ src/firebase/configLoader.js
@@ -0,0 +1,16 @@
+let config;
+
+const isDevelopment = () => process.env.NODE_ENV !== 'production';
+
+if (isDevelopment()) {
+    try {
+        config = require('./firebase-config.dev');
+    } catch (e) {
+        console.error("No dev config. Create a firebase-config.dev.js file with your own firebase project.");
+    }
+} else {
+    config = require('./firebase-config.prod');
+}
+
+export default config;
+
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,17 +1,11 @@
-import devConfig from './firebase-config.dev';
-import prodConfig from './firebase-config.prod';
-
 import firebase from 'firebase';
+import config from './configLoader';
+
 
 class Firebase {
 
     constructor() {
-        const config = Firebase.isDevelopment() ? devConfig : prodConfig;
         this.fire = firebase.initializeApp(config);
-    }
-
-    static isDevelopment() {
-        return process.env.NODE_ENV !== 'production';
     }
 
     auth() {
