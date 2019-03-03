c0a2754e727be856e48e9654788a96617372d40e
tduong992 <tduong992@gmail.com>
2019-03-03 23:43:49 +0100

    Add dev config

---
+++ src/firebase/firebase-config.dev.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyB49zT7KVrVGXrvhAcVd47cQXV9wsVeDLo",
+    authDomain: "fir-and-react-55a5a.firebaseapp.com",
+    databaseURL: "https://fir-and-react-55a5a.firebaseio.com",
+    projectId: "fir-and-react-55a5a",
+    storageBucket: "fir-and-react-55a5a.appspot.com",
+    messagingSenderId: "242011761721"
+};
+
+export default config;
