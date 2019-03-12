597776a4c9b47ac80d114fd0300577649d083b4a
tduong992 <tduong992@gmail.com>
2019-03-12 02:22:10 +0100

    Make App.js 2nd top most React app access point aware of AppNavigation router

--- src/main/App.js
+++ src/main/App.js
@@ -1,58 +1,20 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import AppNavigation from './routing/Router';
 import './App.css';
-import Comment from './Comment/Comment';
 
 
+// NB: this is the 2nd top most access point to the React frontend application
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            messages: [ 'item1', 'item2' ],
-        }
-    }
-
-    onMessageAdd = (message) => {
-        const messages = this.state.messages.slice();
-        messages.push(message);
-
-        this.setState({ messages });
-    };
-
     render() {
-        const { messages } = this.state;
 
         return (
-            <div className='App'>
-                <header className='App-header'>
-                    <img src={ logo } className='App-logo' alt='logo'/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className='App-link'
-                        href='https://reactjs.org'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        Learn React
-                    </a>
-                </header>
-
-                <div style={ { padding: '25px 0', display: 'flex', flexDirection: 'row', justifyContent: 'center' } }>
-                    <div>
-                        <Comment saveMessage={ this.onMessageAdd }/>
-                    </div>
-                    <div>
-                        <ul>
-                            { messages.map((el) => <li key={ el }>{ el }</li>) }
-                        </ul>
-                    </div>
+            <div>
+                <div className="App-content">
+                    <AppNavigation/>
                 </div>
 
-                <footer className='App-footer'>
+                <footer className="App-footer">
                     <p>Product from PSIT4 ZHAW course</p>
                     <p>&copy;LaMaS2019</p>
                 </footer>
@@ -60,5 +22,6 @@ class App extends Component {
         );
     }
 }
+
 
 export default App;
