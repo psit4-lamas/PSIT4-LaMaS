ba2fa1de147d6fa936ecd28dc6e0c3c895720f9f
tduong992 <tduong992@gmail.com>
2019-03-12 02:55:15 +0100

    Put TODO to fix matching TopMenu clicked item with Route in LandingPage.jsx

--- src/main/pages/LandingPage.jsx
+++ src/main/pages/LandingPage.jsx
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import TopMenu from '../ComponentMenu/TopMenu';
+import LectureBodyContent from '../LectureComponents/LecturesContainer';
+import { Route } from 'react-router-dom';
 
 
 class LandingPage extends Component {
 
-    // TODO: improve landing page UI
+    // TODO: improve landing page UI (Sprint 2)
     render() {
+        console.log('LandingPage ', this.props.base);
         return (
-            <div>
+            <React.Fragment>
                 <header>
                     <TopMenu/>
                 </header>
 
-                <main>Llamacorn</main>
+                {/*TODO: fix matching TopMenu clicked items with Route content shown (below) */}
+                <main>
+                    Llamacorn
+                    <Route exact path={ `${ this.props.base }/:subj` } render={ ({ match }) => <LectureBodyContent match={ match }/> }/>
+                </main>
 
                 <footer>Feet</footer>
-            </div>
+            </React.Fragment>
         );
     }
 }
