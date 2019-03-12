459ca1a7b181ab20a549d48c5170768bbfb62e3e
tduong992 <tduong992@gmail.com>
2019-03-12 03:01:21 +0100

    Add UploadMediaPage .jsx and .css with TODOs

---
+++ src/main/pages/UploadMediaPage.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './UploadMediaPage.css';
+import TopMenu from '../ComponentMenu/TopMenu';
+
+
+class UploadMediaPagePage extends Component {
+
+    // TODO: improve upload media page UI
+    render() {
+
+        return (
+            <React.Fragment>
+                <header><TopMenu/></header>
+
+                <main>Llamacorn!</main>
+
+                <footer>Feet</footer>
+            </React.Fragment>
+        );
+    }
+}
+
+
+export default UploadMediaPagePage;
