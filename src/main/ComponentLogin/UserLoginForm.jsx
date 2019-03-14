3219af0eea5b443da2eae5bb9642b48a5183d5db
waedi <martinw@gmx.ch>
2019-03-14 13:38:13 +0100

    made login page more pretty and added interesting color choice

--- src/main/ComponentLogin/UserLoginForm.jsx
+++ src/main/ComponentLogin/UserLoginForm.jsx
@@ -1,14 +1,12 @@
-import React, { Component, useState } from 'react';
-import { connect } from 'react-redux';
-import { logIn } from '../actions';
-import { Button, Checkbox, Form, Input } from 'semantic-ui-react';
-
+import React, {Component, useState} from 'react';
+import {connect} from 'react-redux';
+import {logIn} from '../actions';
+import {Button, Form, Input, Segment} from 'semantic-ui-react';
 
 const FormField = Form.Field;
 
 
 class UserLoginForm extends Component {
-
     onSubmit = (email, password) => {
         this.props.logIn(email, password);
     };
@@ -16,31 +14,40 @@ class UserLoginForm extends Component {
     render() {
         return (
             <div>
-                <LoginFormComponent onSubmit={ this.onSubmit }/>
+                <LoginFormComponent onSubmit={this.onSubmit}/>
             </div>
         );
     }
 }
 
 
-const LoginFormComponent = ({ onSubmit }) => {
+const LoginFormComponent = ({onSubmit}) => {
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
 
     return (
-        <Form onSubmit={ () => onSubmit(email, password) }>
-            <FormField>
-                <label>Email</label>
-                <Input name={ 'email' } placeholder="test@zhaw.ch" value={ email } onChange={ (e) => setEmail(e.target.value) }/>
-            </FormField>
-            <FormField>
-                <label>Password</label>
-                <Input name={ 'password' } type={ 'password' } value={ password } onChange={ (e) => setPassword(e.target.value) }/>
-            </FormField>
-            <FormField>
-                <Checkbox label="I agree to the Terms and Conditions"/>
-            </FormField>
-            <Button type="submit">Submit</Button>
+        <Form size="large" onSubmit={() => onSubmit(email, password)}>
+            <Segment stacked>
+                <FormField>
+                    <Input fluid icon="user" iconPosition="left" name={'email'} placeholder="e-mail address" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                </FormField>
+                <FormField>
+                    <Input
+                        fluid
+                        icon="lock"
+                        iconPosition="left"
+                        name={'password'}
+                        placeholder="password"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </FormField>
+
+                <Button type="submit" color="pink" fluid size="large">
+                    Login
+                </Button>
+            </Segment>
         </Form>
     );
 };
@@ -51,4 +58,7 @@ const mapDispatchToProps = {
     logIn,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(UserLoginForm);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(UserLoginForm);
--- src/main/pages/LoginPage.jsx
+++ src/main/pages/LoginPage.jsx
@@ -1,19 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import UserLoginForm from '../ComponentLogin/UserLoginForm';
 import './LoginPage.css';
+import {Grid, Header, Image} from 'semantic-ui-react';
 
 
 class LoginPage extends Component {
-
     // TODO: improve Login page UI
     render() {
-
         return (
-            <React.Fragment>
-                <p>You are not logged in!</p>
-
-                <UserLoginForm/>
-            </React.Fragment>
+            <Grid textAlign="center" style={{height: '100%'}} verticalAlign="middle">
+                <Grid.Column style={{maxWidth: 450}}>
+                    <Header as="h2" color="pink" textAlign="center">
+                        <Image
+                            src="https://firebasestorage.googleapis.com/v0/b/fir-and-react-55a5a.appspot.com/o/images%2Flama_round_draft.png?alt=media&token=c57d37c7-efc2-4a06-99d2-25d5bf2ed443"/>{' '}
+                        Log-in to your account
+                    </Header>
+                    <React.Fragment>
+                        <UserLoginForm/>
+                    </React.Fragment>
+                </Grid.Column>
+            </Grid>
         );
     }
 }
