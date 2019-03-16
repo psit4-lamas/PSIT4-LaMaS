e0f9c72ee0ce180cf6d69304893b0fb76ea9bc22
kernda02 <kernda02@students.zhaw.ch>
2019-03-16 22:33:48 +0100

    added Logout Button

--- src/main/ComponentMenu/TopMenu.jsx
+++ src/main/ComponentMenu/TopMenu.jsx
@@ -1,32 +1,32 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
 import i18n from '../../i18n';
-import { withNamespaces } from 'react-i18next';
-import { Dropdown, Input, Menu, Segment } from 'semantic-ui-react';
+import {withNamespaces} from 'react-i18next';
+import {Dropdown, Input, Menu, Segment, Button} from 'semantic-ui-react';
 import './TopMenu.css';
 
 
 class TopMenu extends Component {
 
-    state = { activeItem: 'home' };
+    state = {activeItem: 'home'};
 
     changeLanguage = (lang) => {
         i18n.changeLanguage(lang);
     };
 
-    handleItemClick = (e, { name }) => {
-        this.setState({ activeItem: name });
+    handleItemClick = (e, {name}) => {
+        this.setState({activeItem: name});
         this.props.history.push(name);
     };
 
     render() {
-        const { activeItem } = this.state;
-        const { t } = this.props;
+        const {activeItem} = this.state;
+        const {t} = this.props;
         // TODO: The activeTabs is a list of subjects defined in src/main/reducers/index.js
         //       It simulates fetching user's bookmarked subjects from backend based on logged in user.
         //       The backend feature is not implemented at the moment, thus define a mock data in reducers/index.js
-        const { activeTabs } = this.props.tabs;
+        const {activeTabs} = this.props.tabs;
 
         return (
             <div>
@@ -50,6 +50,14 @@ class TopMenu extends Component {
                     <Menu.Menu position="right">
                         <Menu.Item>
                             <Input icon="search" placeholder="Search..."/>
+                        </Menu.Item>
+                    </Menu.Menu>
+
+                    <Menu.Menu position="right">
+                        <Menu.Item>
+                            <Button>
+                                Logout
+                            </Button>
                         </Menu.Item>
                     </Menu.Menu>
                 </Menu>
