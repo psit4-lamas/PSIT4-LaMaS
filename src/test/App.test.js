dbfe143b214be3bc98501cea0f89e30583d07139
waedenma <waedenma@students.zhaw.ch>
2019-03-16 22:37:05 +0100

    Added renders without crashing test

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -8,3 +8,10 @@ it('renders without crashing', () => {
 
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<UserLoginForm />, div);
+
+  ReactDOM.unmountComponentAtNode(div);
+});
