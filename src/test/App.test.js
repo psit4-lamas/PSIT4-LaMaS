65537607abd05016a30d840346708902666373f6
wueesmar <wueesmar@students.zhaw.ch>
2019-03-17 21:40:50 +0100

    user story login
    - fixed error in test

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../main/App';
+import UserLoginForm from '../main/ComponentLogin/UserLoginForm';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
