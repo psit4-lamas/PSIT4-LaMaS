23d7e03684bebf91a6e8963a4916fd8a5c99b698
tduong992 <tduong992@gmail.com>
2019-03-17 23:34:09 +0100

    Adapt routes to fix the redirect issue

--- src/main/routing/LoginGuard.jsx
+++ src/main/routing/LoginGuard.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import { userRedirectedToAccessedPath } from '../actions';
 
 const LoginGuard = (props) => {
 
     if (!props.user.isAuthenticated) {
         return props.children;
+    }
+
+    // Now the user just finished the login process. If the user wanted to access a bookmarked link,
+    // redirect to the path she/he requested before being redirected to the login page.
+    const { userAccessedPathname } = props.user;
+    if (userAccessedPathname) {
+        props.userRedirectedToAccessedPath();
+        return <Redirect to={ userAccessedPathname } />;
     }
 
     return (
@@ -23,4 +31,8 @@ const mapStateToProps = (state) => ( {
     user: state.user,
 } );
 
-export default connect(mapStateToProps, {})(LoginGuard);
+const mapDispatchToProps = {
+    userRedirectedToAccessedPath,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginGuard);
--- src/main/routing/SecureGuard.jsx
+++ src/main/routing/SecureGuard.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import { userRedirectedToAccessedPath } from '../actions';
 
 const SecureGuard = (props) => {
 
     if (props.user.isAuthenticated) {
+
+        // If the authenticated user triggers a page refresh, clear first the stored accessed path name.
+        if (props.user.userAccessedPathname) {
+            props.userRedirectedToAccessedPath();
+        }
+
         return props.children;
     }
 
@@ -23,4 +29,8 @@ const mapStateToProps = (state) => ( {
     user: state.user,
 } );
 
-export default connect(mapStateToProps, {})(SecureGuard);
+const mapDispatchToProps = {
+    userRedirectedToAccessedPath,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SecureGuard);
