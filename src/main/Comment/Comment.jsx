b76f2eb1e32178eb2a5729118e6d3fe3ab985b49
aleksandar-spasojevic <aleksandar.spasojevic@outlook.com>
2019-03-17 23:19:27 +0100

    add test for Comment Component

--- src/main/Comment/Comment.jsx
+++ src/main/Comment/Comment.jsx
@@ -1,22 +1,28 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
-const Comment = ({ saveMessage }) => {
-    const [value, setValue] = useState('');
 
-    return (
-        <form
-            onSubmit={ (event) => {
-                event.preventDefault();
-                saveMessage(value);
-                setValue('');
-            } }
-        >
-            <input
-                onChange={ (event) => setValue(event.target.value) }
-                value={ value }
-            />
-        </form>
-    );
-};
+class Comment extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = { value: '' };
+    }
+
+    render() {
+        let { saveMessage } = this.props;
+
+        return (
+            <form
+                onSubmit={ (event) => {
+                    event.preventDefault();
+                    saveMessage(this.state.value);
+                    this.setState({ value: '' });
+                } }
+            >
+                <input onChange={ (event) => this.setState({ value: event.target.value }) } value={ this.state.value }/>
+            </form>
+        );
+    }
+}
+
 
 export default Comment;
