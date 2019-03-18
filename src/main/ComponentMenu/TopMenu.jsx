143ae8335e7df8bc5777dc9615a81e08004e7330
tduong992 <tduong992@gmail.com>
2019-03-18 01:23:46 +0100

    Fix Menu pointing to the page content

--- src/main/ComponentMenu/TopMenu.jsx
+++ src/main/ComponentMenu/TopMenu.jsx
@@ -15,9 +15,9 @@ class TopMenu extends Component {
     };
 
     render() {
-        const { activeItem } = this.state;
         const { t, changeLanguage, activeTabs } = this.props;
         const currentPathname = window.location.pathname;
+        const currentName = currentPathname.replace('/courses/', '').replace('%20', ' ');
         // TODO: The activeTabs is a list of subjects defined in src/main/reducers/index.js
         //       It simulates fetching user's bookmarked subjects from backend based on logged in user.
         //       The backend feature is not implemented at the moment, thus define a mock data in reducers/index.js
@@ -25,11 +25,11 @@ class TopMenu extends Component {
         return (
             <div>
                 <Menu pointing id='top-menu'>
-                    <Menu.Item name="/home" active={ activeItem === 'home' } onClick={ this.handleItemClick }/>
-                    <Menu.Item name="/upload" active={ activeItem === 'upload' } onClick={ this.handleItemClick }/>
+                    <Menu.Item name="/home" active={ currentName === 'home' } onClick={ this.handleItemClick }/>
+                    <Menu.Item name="/upload" active={ currentName === '/upload' } onClick={ this.handleItemClick }/>
 
                     { activeTabs.map((activeTab, index) => (
-                        <Menu.Item key={ activeTab } name={ activeTab } active={ activeItem === activeTab } onClick={ this.handleItemClick }/>
+                        <Menu.Item key={ activeTab } name={ activeTab } active={ currentName === activeTab } onClick={ this.handleItemClick }/>
                     )) }
 
                     <Menu.Menu position="right">
