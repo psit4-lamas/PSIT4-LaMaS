7857fe8a0981409a38285a384d9fbe00ba08e000
tduong992 <tduong992@gmail.com>
2019-03-18 14:11:30 +0100

    Move UserLoginForm test into separated test file

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../main/App';
-import UserLoginForm from '../main/ComponentLogin/UserLoginForm';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<UserLoginForm />, div);
 
   ReactDOM.unmountComponentAtNode(div);
 });
