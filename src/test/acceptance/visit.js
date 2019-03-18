790778aea861d1775071b4a8df0a174365bc4f48
Aleksandar Spasojevic <aleksandar.spasojevic@outlook.com>
2019-03-18 13:39:19 +0100

    change port

--- src/test/acceptance/visit.js
+++ src/test/acceptance/visit.js
@@ -4,7 +4,7 @@ import url from 'url'
 const BASE_URL = url.format({
     protocol : process.env.PROTOCOL || 'http',
     hostname : process.env.HOST || 'localhost',
-    port     : process.env.PORT || 3000
+    port     : process.env.PORT || 9000
 });
 
 const DEBUG = process.env.DEBUG || false;
