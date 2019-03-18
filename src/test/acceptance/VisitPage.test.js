dd92f52332a8e111be01d371efbbb899a4668328
Aleksandar Spasojevic <aleksandar.spasojevic@outlook.com>
2019-03-18 13:29:04 +0100

    PoC nightmare

---
+++ src/test/acceptance/VisitPage.test.js
@@ -0,0 +1,13 @@
+import visit from './visit'
+
+describe('When visiting the homepage', function () {
+
+    test('it welcomes the user', async function () {
+        let page = visit('/');
+        let text = await page.evaluate(() => document.body.textContent)
+            .end();
+
+        expect(text).toContain('Welcome to React')
+    });
+
+});
