8100d9fdd1ceeaca6a5d3b2d29a2cf7f78c960bc
tduong992 <tduong992@gmail.com>
2019-03-18 14:11:06 +0100

    Cleanups, reorder imports (CSS always as last import statements)

--- src/main/ComponentLogin/UserLoginForm.jsx
+++ src/main/ComponentLogin/UserLoginForm.jsx
@@ -1,26 +1,27 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {logIn} from '../actions';
-import {Button, Form, Input, Segment} from 'semantic-ui-react';
-import {withNamespaces} from 'react-i18next';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { logIn } from '../actions';
+import { withNamespaces } from 'react-i18next';
+import { Button, Form, Input, Segment } from 'semantic-ui-react';
 
 const FormField = Form.Field;
 
 
 class UserLoginForm extends Component {
+
     onSubmit = (email, password) => {
         this.props.logIn(email, password);
     };
 
     render() {
-        const {t} = this.props;
+        const { t } = this.props;
         return (
             <div>
                 <Form size="large" onSubmit={ () => this.onSubmit(this.state.email, this.state.password) }>
                     <Segment stacked>
                         <FormField>
                             <Input fluid icon="user" iconPosition="left" name={ 'email' } placeholder={ t('login.email') }
-                                   onChange={ (e) => this.setState({email: e.target.value}) }/>
+                                   onChange={ (e) => this.setState({ email: e.target.value }) }/>
                         </FormField>
                         <FormField>
                             <Input
@@ -30,7 +31,7 @@ class UserLoginForm extends Component {
                                 name={ 'password' }
                                 placeholder={ t('login.password') }
                                 type="password"
-                                onChange={ (e) => this.setState({password: e.target.value}) }
+                                onChange={ (e) => this.setState({ password: e.target.value }) }
                             />
                         </FormField>
 
@@ -51,9 +52,5 @@ const mapDispatchToProps = {
     logIn,
 };
 
-export default withNamespaces()(
-    connect(
-        mapStateToProps,
-        mapDispatchToProps,
-    )(UserLoginForm),
-);
+export { UserLoginForm }
+export default withNamespaces()(connect(mapStateToProps, mapDispatchToProps)(UserLoginForm));
--- src/main/pages/LoginPage.jsx
+++ src/main/pages/LoginPage.jsx
@@ -1,22 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { withNamespaces } from 'react-i18next';
+import { Grid, Header, Image } from 'semantic-ui-react';
+import logo_round from '../../images/lama_round_draft.png';
 import UserLoginForm from '../ComponentLogin/UserLoginForm';
 import './LoginPage.css';
-import {Grid, Header, Image} from 'semantic-ui-react';
-import logo_round from '../../images/lama_round_draft.png'
-import {withNamespaces} from 'react-i18next';
 
 
 class LoginPage extends Component {
     // TODO: improve Login page UI
     render() {
-        const {t} = this.props;
+        const { t } = this.props;
+
         return (
-            <Grid textAlign="center" style={ {height: '100%'} } verticalAlign="middle">
-                <Grid.Column style={ {maxWidth: 450} }>
+            <Grid textAlign="center" style={ { height: '100%' } } verticalAlign="middle">
+                <Grid.Column style={ { maxWidth: 450 } }>
                     <Header as="h2" color="pink" textAlign="center">
-                        <Image
-                            src={ logo_round }/>{ ' ' }
-                        { t('login.message') }
+                        <Image src={ logo_round }/> { t('login.message') }
                     </Header>
                     <React.Fragment>
                         <UserLoginForm/>
