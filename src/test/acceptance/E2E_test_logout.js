8839a8bb73ad2dbb8ce30914a00c50e2cc4b3ac8
kernda02 <kernda02@students.zhaw.ch>
2019-03-27 18:29:35 +0100

    End-to-End test init.

---
+++ src/test/acceptance/E2E_test_logout.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    'TestLogout' : function (browser) {
+        browser
+            .url('http://localhost:3000/login')
+            .waitForElementVisible('body', 10000)
+            //.setValue('input[type=text]', 'nightwatch js')
+            //.waitForElementVisible('input[name=btnK]')
+            //.click('input[name=btnK]')nigh
+            .pause(1000)
+            //.assert.containsText('.LC20lb', 'Nightwatch.js | Node.js powered End-to-End testing framework')
+            .end();
+    }
+};
