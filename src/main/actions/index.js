b36fbc2c77115eeb24700c1504791d4238c5562d
tduong992 <tduong992@gmail.com>
2019-03-30 15:01:37 +0100

    Add comments describing actions' purposes, fix duplicated code

--- src/main/actions/index.js
+++ src/main/actions/index.js
@@ -1,5 +1,6 @@
 import firebase from '../../firebase';
-import config, {isDevelopment} from '../../firebase/configLoader';
+import config, { isDevelopment } from '../../firebase/configLoader';
+
 
 const Actions = {
     LOAD_USER: 'LOAD_USER',
@@ -38,32 +39,35 @@ const userRedirectedToAccessedPath = () => {
 
 const subscribeToAuthStateChanged = () => {
     return (dispatch) => {
-        firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                console.log(user);
+        firebase.auth()
+                .onAuthStateChanged((user) => {
+                    if (user) {
+                        console.log(user);
 
-                // If the user has not confirmed his/her account yet, re-send a confirmation email
-                // with a 'Continue' link, redirecting the user to the LaMaS web application
-                if (!user.emailVerified) {
-                    const redirectURI = isDevelopment() ? 'http://localhost:3000' : `https://${ config.default.authDomain }`;
+                        // If the user has not confirmed his/her account yet, re-send a confirmation email
+                        // with a 'Continue' link, redirecting the user to the LaMaS web application
+                        if (!user.emailVerified) {
+                            const redirectURI = isDevelopment()
+                                                ? 'http://localhost:3000'
+                                                : `https://${ config.default.authDomain }`;
 
-                    user.sendEmailVerification({
-                        url: redirectURI,
-                    });
-                }
+                            user.sendEmailVerification({
+                                url: redirectURI,
+                            });
+                        }
 
-                dispatch({
-                    type: Actions.LOG_IN_SUCCESS,
-                    payload: user,
+                        dispatch({
+                            type: Actions.LOG_IN_SUCCESS,
+                            payload: user,
+                        });
+                    } else {
+                        console.log('User logged out!');
+
+                        dispatch({
+                            type: Actions.LOG_OUT_SUCCESS,
+                        });
+                    }
                 });
-            } else {
-                console.log('User logged out!');
-
-                dispatch({
-                    type: Actions.LOG_OUT_SUCCESS,
-                });
-            }
-        });
     };
 };
 
@@ -91,7 +95,7 @@ const logOut = () => {
             .auth()
             .signOut()
             .then((res) => {
-                dispatch({type: Actions.LOG_OUT_SUCCESS});
+                dispatch({ type: Actions.LOG_OUT_SUCCESS });
             })
             .catch((err) => {
                 console.log('ERROR ON LOGOUT ', err);
@@ -99,6 +103,14 @@ const logOut = () => {
     };
 };
 
+/**
+ * Load the requested subject document.
+ *
+ * This action is called when a user clicks on a subject's link, requesting the subject's content.
+ *
+ * @param subjectId     The subject ID to be fetched from firestore
+ * @returns {Function}
+ */
 const loadSubject = (subjectId) => {
     return (dispatch) => {
         firebase
@@ -116,34 +128,49 @@ const loadSubject = (subjectId) => {
     };
 };
 
+/**
+ * Load all bookmarked subjects' links of the logged in user.
+ *
+ * This action is called on LandingPage to fire a request fetching all current user's bookmarked subjects' links.
+ *
+ * @returns {Function}
+ */
 const loadSubjectHead = () => {
     return (dispatch) => {
+        // TODO: at the moment this request just fetch all subjects' names available on firestore!
+        //       Later we will just fetch those subjects' names that the specific user (student (tutor too?)) has bookmarked!
         firebase
             .database()
             .collection('subjects')
             .onSnapshot(function (querySnapshot) {
-                querySnapshot.docChanges().forEach(function (change) {
-                    if (change.type === 'added') {
-                        dispatch({
-                            type: Actions.SUBJECT_INSERT_HEAD,
-                            payload: {
-                                name: change.doc.data().subject_name,
-                                subjectId: change.doc.id,
-                            },
-                        });
-                    }
-                    if (change.type === 'removed') {
-                        dispatch({
-                            type: Actions.SUBJECT_REMOVE_HEAD,
-                            payload: {
-                                name: change.doc.data().subject_name,
-                                subjectId: change.doc.id,
-                            },
-                        });
-                    }
-                });
+                querySnapshot.docChanges()
+                             .forEach(function (change) {
+                                 const response = {
+                                         name: change.doc.data().subject_name,
+                                         subjectId: change.doc.id,
+                                     };
+
+                                 if (change.type === 'added') {
+                                     dispatch({
+                                         type: Actions.SUBJECT_INSERT_HEAD,
+                                         payload: response,
+                                     });
+                                 }
+
+                                 if (change.type === 'removed') {
+                                     dispatch({
+                                         type: Actions.SUBJECT_REMOVE_HEAD,
+                                         payload: response,
+                                     });
+                                 }
+                             });
             });
     };
 };
 
-export {Actions, loadUser, userRedirectedToAccessedPath, subscribeToAuthStateChanged, logIn, logOut, loadSubject, loadSubjectHead};
+export {
+    Actions,
+    loadUser, userRedirectedToAccessedPath, subscribeToAuthStateChanged,
+    logIn, logOut,
+    loadSubject, loadSubjectHead,
+};
