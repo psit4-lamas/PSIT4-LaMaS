f109dd71e18e54492d6f3ca780c10e2bacc8c579
kernda02 <kernda02@students.zhaw.ch>
2019-03-30 16:58:28 +0100

    fixed Login Test

--- src/test/acceptance/E2E_test_login.js
+++ src/test/acceptance/E2E_test_login.js
@@ -1,8 +1,10 @@
 
 module.exports = {
 
+
+    // TODO: set application address using a global placeholder
     // login using valid credentials
-    'Step Login' : function (browser) {
+    'Step Login with valid credentials' : function (browser) {
         browser
             .url('http://localhost:3000')
             .waitForElementVisible('body', 10000)
@@ -11,12 +13,27 @@ module.exports = {
             .setValue('input[name=password]', 'dummy1')
             .click('button[type=submit]')
             .pause(3000)
-            // assume Login was correct if topMenu is visible
-            .assert.visible('#top-menu')
+            // assume Login was correct if topMenu Logout Button is visible
+            .assert.elementPresent('#top-menu-logout')
+            .assert.visible('#top-menu-logout')
             .end();
     },
 
-
+    // login using valid credentials, i.e not a ZHAW email address
+    'Step Login with invalid credentials' : function (browser) {
+        browser
+            .url('http://localhost:3000')
+            .waitForElementVisible('body', 10000)
+            .waitForElementVisible('input[name=email]')
+            .setValue('input[type=text]', 'someone@somedomain.com')
+            .setValue('input[name=password]', 'someone')
+            .click('button[type=submit]')
+            .pause(3000)
+            // TODO: define Login Error Flow
+            // assume Login was correct if topMenu Logout Button is not present
+            .assert.elementNotPresent('#top-menu-logout')
+            .end();
+    },
 
 
 };
