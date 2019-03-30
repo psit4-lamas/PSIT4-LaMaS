3ffb9f2f66e5dadb510e7ec66ced04631813e780
waedenma <waedenma@students.zhaw.ch>
2019-03-30 23:21:31 +0100

    Fixed 1 test

---
+++ src/firebase/firebase-config.dev.js
@@ -0,0 +1,24 @@
+// Initialize Firebase
+// const config = {
+//     apiKey: "AIzaSyB49zT7KVrVGXrvhAcVd47cQXV9wsVeDLo",
+//     authDomain: "fir-and-react-55a5a.firebaseapp.com",
+//     databaseURL: "https://fir-and-react-55a5a.firebaseio.com",
+//     projectId: "fir-and-react-55a5a",
+//     storageBucket: "fir-and-react-55a5a.appspot.com",
+//     messagingSenderId: "242011761721"
+// };
+//
+// export default config;
+
+
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyDuWk7k3WVtFDIy4IV4hvRHNdtKdOHIw5Y",
+    authDomain: "psit4testproject.firebaseapp.com",
+    databaseURL: "https://psit4testproject.firebaseio.com",
+    projectId: "psit4testproject",
+    storageBucket: "psit4testproject.appspot.com",
+    messagingSenderId: "1077999385094"
+};
+
+export default config;
