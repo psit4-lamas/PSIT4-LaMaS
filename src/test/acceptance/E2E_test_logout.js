d5d5c4eeffe16791a3cc6dadf84a3d0aa45f6a7c
kernda02 <kernda02@students.zhaw.ch>
2019-03-30 17:14:37 +0100

    logout test implemented

--- src/test/acceptance/E2E_test_logout.js
+++ src/test/acceptance/E2E_test_logout.js
@@ -1,16 +1,32 @@
 
 module.exports = {
 
+    beforeEach(browser) {
+
+        browser
+            // login
+            .url('http://localhost:3000')
+            .waitForElementVisible('body', 10000)
+            .waitForElementVisible('input[name=email]')
+            .setValue('input[type=text]', 'dummy1@students.zhaw.ch')
+            .setValue('input[name=password]', 'dummy1')
+            .click('button[type=submit]')
+            .pause(3000)
+            // assume Login was correct if topMenu Logout Button is visible
+            .assert.elementPresent('#top-menu-logout')
+            .assert.visible('#top-menu-logout')
+    },
+
 
     'TestLogout' : function (browser) {
         browser
-            .url('http://localhost:3000')
-            .waitForElementVisible('body', 10000)
-            //.setValue('input[type=text]', 'nightwatch js')
-            //.waitForElementVisible('input[name=btnK]')
-            //.click('input[name=btnK]')nigh
+
+            .waitForElementVisible('#top-menu-logout')
+            .click('#top-menu-logout')
             .pause(1000)
-            //.assert.containsText('.LC20lb', 'Nightwatch.js | Node.js powered End-to-End testing framework')
+            // check if LoginPage becomes visible after Logout
+            .assert.elementPresent('button[type=submit]')
+            .assert.visible('button[type=submit]')
             .end();
     }
 };
