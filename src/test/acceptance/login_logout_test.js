51734207afa55a9b7f3443ea7067bb0f714082cd
aleksandar-spasojevic <aleksandar.spasojevic@outlook.com>
2019-03-31 18:16:17 +0200

    wait longer for elements to be visible

--- src/test/acceptance/login_logout_test.js
+++ src/test/acceptance/login_logout_test.js
@@ -4,18 +4,18 @@ module.exports = {
     'Step Login with valid credentials' : function (browser) {
         browser
             .url('http://localhost:3000')
-            .waitForElementVisible('input[name=email]')
+            .waitForElementVisible('input[name=email]',10000)
             .setValue('input[type=text]', 'dummy1@students.zhaw.ch')
             .setValue('input[name=password]', 'dummy1')
             .click('button[type=submit]')
-            .waitForElementVisible('#top-menu-logout')
+            .waitForElementVisible('#top-menu-logout',10000)
             .end();
     },
 
     'Step Login with invalid credentials' : function (browser) {
         browser
             .url('http://localhost:3000')
-            .waitForElementVisible('input[name=email]')
+            .waitForElementVisible('input[name=email]',10000)
             .setValue('input[type=text]', 'someone@somedomain.com')
             .setValue('input[name=password]', 'someone')
             .click('button[type=submit]')
@@ -29,15 +29,15 @@ module.exports = {
     'Logout' : function (browser) {
         browser
             .url('http://localhost:3000')
-            .waitForElementVisible('input[name=email]')
+            .waitForElementVisible('input[name=email]',10000)
             .setValue('input[type=text]', 'dummy1@students.zhaw.ch')
             .setValue('input[name=password]', 'dummy1')
             .click('button[type=submit]')
             // assume Login was correct if topMenu Logout Button is visible
-            .waitForElementVisible('#top-menu-logout')
+            .waitForElementVisible('#top-menu-logout',10000)
             .click('button#logout')
             // check if LoginPage becomes visible after Logout
-            .waitForElementVisible('button[type=submit]')
+            .waitForElementVisible('button[type=submit]',10000)
             .end();
     }
 };
