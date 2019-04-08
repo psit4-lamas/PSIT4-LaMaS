c11d571ba4ecc05e7e1d04b4e93ea79f7380efbe
tduong992 <tduong992@gmail.com>
2019-04-09 01:39:43 +0200

    Improve CreateSubject tests using before-/afterEach() methods

--- src/test/ComponentCreateSubject/CreateSubject.test.js
+++ src/test/ComponentCreateSubject/CreateSubject.test.js
@@ -13,79 +13,74 @@ describe('CreateSubject', () => {
         ReactDOM.unmountComponentAtNode(div);
     });
 
-    it('sets subject state value if subject is entered', () => {
-        const responseSubject = {};
-        const createSubjectComponent = shallow(<CreateSubject t={ (key) => key } responseSubject={ responseSubject }/>);
-        const event = { target: { name: 'subject', value: 'mySubject' } };
 
-        createSubjectComponent.find({ name: 'subject' }).simulate('change', event);
-        expect(createSubjectComponent.state('subject')).toEqual(event.target.value);
+    let createSubjectComponent;
+    let createSubject;
+    let responseSubject;
 
+    const subjectEvent = Object.freeze({ target: { name: 'subject', value: 'SubjectName' } });
+    const tutorsDefault = Object.freeze({ target: null });
+    const tutorsEvent = Object.freeze({ value: ['TutorName'] });
+
+    beforeEach(() => {
+        createSubject = jest.fn();
+        responseSubject = {};
+
+        const component = (
+            <CreateSubject
+                t={ (key) => key }
+                createSubject={ createSubject }
+                responseSubject={ responseSubject }
+            />);
+
+        createSubjectComponent = shallow(component);
+    });
+
+    afterEach(() => {
         createSubjectComponent.unmount();
+    });
+
+    it('should render correctly', () => {
+        expect(createSubjectComponent).toMatchSnapshot();
+    });
+
+    it('sets subject state value if subject is entered', () => {
+        createSubjectComponent.find({ name: 'subject' }).simulate('change', subjectEvent);
+        expect(createSubjectComponent.state('subject')).toEqual(subjectEvent.target.value);
     });
 
     it('sets selectedTutors state value if tutors are entered', () => {
-        const responseSubject = {};
-        const createSubjectComponent = shallow(<CreateSubject t={ (key) => key } responseSubject={ responseSubject }/>);
-        const changeEvent = { target: null };
-        const changeValue = { value: ['TutorName'] };
-
-        createSubjectComponent.find('Dropdown').prop('onChange')(changeEvent, changeValue);
-        expect(createSubjectComponent.state('selectedTutors')).toEqual(['TutorName']);
-
-        createSubjectComponent.unmount();
+        createSubjectComponent.find('Dropdown').prop('onChange')(tutorsDefault, tutorsEvent);
+        expect(createSubjectComponent.state('selectedTutors')).toEqual(tutorsEvent.value);
     });
 
-    it('calls createSubject if Save button is clicked', () => {
-        const createSubject = jest.fn();
-        const responseSubject = { subject_id: '01234' };
-        const createSubjectComponent = shallow(<CreateSubject t={ (key) => key } createSubject={ createSubject } responseSubject={ responseSubject }/>);
+    it('calls createSubject if Save button is clicked with valid form values', () => {
         const subjectfield = createSubjectComponent.find({ name: 'subject' });
         const tutorfield = createSubjectComponent.find({ name: 'tutors' });
 
-        const subjectEvent = { target: { value: 'SubjectName' } };
-        const tutorsEvent = { target: null };
-        const changeValue = { value: ['TutorName'] };
-
         subjectfield.simulate('change', subjectEvent);
-        tutorfield.prop('onChange')(tutorsEvent, changeValue);
+        tutorfield.prop('onChange')(tutorsDefault, tutorsEvent);
         createSubjectComponent.find(Form).simulate('submit');
 
-        expect(createSubject).toHaveBeenCalledWith(subjectEvent.target.value, changeValue.value);
-
-        createSubjectComponent.unmount();
+        expect(createSubject).toHaveBeenCalledWith(subjectEvent.target.value, tutorsEvent.value);
     });
 
     it('updates state if Save button is clicked', () => {
-        const createSubject = jest.fn();
-        const responseSubject = { subject_id: '01234' };
-        const createSubjectComponent = shallow(<CreateSubject t={ (key) => key } createSubject={ createSubject } responseSubject={ responseSubject }/>);
         const subjectfield = createSubjectComponent.find({ name: 'subject' });
         const tutorfield = createSubjectComponent.find({ name: 'tutors' });
 
-        const subjectEvent = { target: { value: 'SubjectName' } };
-        const tutorsEvent = { target: null };
-        const changeValue = { value: ['TutorName'] };
-
         subjectfield.simulate('change', subjectEvent);
-        tutorfield.prop('onChange')(tutorsEvent, changeValue);
+        tutorfield.prop('onChange')(tutorsDefault, tutorsEvent);
         createSubjectComponent.find(Form).simulate('submit');
 
         expect(createSubjectComponent.state('subject')).toEqual('');
         expect(createSubjectComponent.state('selectedTutors')).toEqual([]);
         expect(createSubjectComponent.state('submittedSubject')).toEqual(subjectEvent.target.value);
-        expect(createSubjectComponent.state('submittedTutors')).toEqual(changeValue.value);
-
-        createSubjectComponent.unmount();
+        expect(createSubjectComponent.state('submittedTutors')).toEqual(tutorsEvent.value);
     });
 
-    it('prevents user from updating state if Save button is clicked and a form item value is missing', () => {
-        const createSubject = jest.fn();
-        const responseSubject = {};
-        const createSubjectComponent = shallow(<CreateSubject t={ (key) => key } createSubject={ createSubject } responseSubject={ responseSubject }/>);
+    it('prevents user from submitting with missing form value', () => {
         const subjectfield = createSubjectComponent.find({ name: 'subject' });
-
-        const subjectEvent = { target: { value: 'SubjectName' } };
 
         subjectfield.simulate('change', subjectEvent);
         createSubjectComponent.find(Form).simulate('submit');
@@ -96,18 +91,6 @@ describe('CreateSubject', () => {
         expect(createSubjectComponent.state('submittedTutors')).toEqual([]);
 
         expect(createSubject).not.toHaveBeenCalled();
-
-        createSubjectComponent.unmount();
-    });
-
-    it('should render correctly', () => {
-        const createSubject = jest.fn();
-        const responseSubject = {};
-        const createSubjectComponent = shallow(<CreateSubject t={ (key) => key } createSubject={ createSubject } responseSubject={ responseSubject }/>);
-
-        expect(createSubjectComponent).toMatchSnapshot();
-
-        createSubjectComponent.unmount();
     });
 
     it('displays success message box on success', () => {
@@ -116,7 +99,7 @@ describe('CreateSubject', () => {
             subject_id: 123,
         };
 
-        const createSubjectComponent = mount(<CreateSubject t={ (key) => key } responseSubject={ responseSubject }/>);
+        createSubjectComponent = mount(<CreateSubject t={ (key) => key } responseSubject={ responseSubject }/>);
 
         expect(createSubjectComponent.find('.ui.success.message').get(0)).toBeTruthy();
         expect(createSubjectComponent.find('.ui.negative.message').get(0)).toBeFalsy();
@@ -127,7 +110,8 @@ describe('CreateSubject', () => {
             isSubmitted: true,
             subject_id: null,
         };
-        const createSubjectComponent = mount(<CreateSubject t={ (key) => key } responseSubject={ responseSubject }/>);
+
+        createSubjectComponent = mount(<CreateSubject t={ (key) => key } responseSubject={ responseSubject }/>);
 
         expect(createSubjectComponent.find('.ui.success.message').get(0)).toBeFalsy();
         expect(createSubjectComponent.find('.ui.negative.message').get(0)).toBeTruthy();
@@ -146,16 +130,12 @@ describe('CreateSubject', () => {
             { key: 'Renate Kummer', text: 'Renate Kummer', value: 'Renate Kummer' },
         ];
         const responseSubject = {};
-        const createSubjectComponent = shallow(<CreateSubject t={ (key) => key } responseSubject={ responseSubject } availableTutors={ availableTutorsBefore }/>);
+        createSubjectComponent = shallow(<CreateSubject t={ (key) => key } responseSubject={ responseSubject } availableTutors={ availableTutorsBefore }/>);
         const tutorfield = createSubjectComponent.find({ name: 'tutors' });
 
-        const tutorsEvent = { target: null };
         const changeValue = { value: 'TutorName' };
-
-        tutorfield.prop('onAddItem')(tutorsEvent, changeValue);
+        tutorfield.prop('onAddItem')(tutorsDefault, changeValue);
 
         expect(createSubjectComponent.state('availableTutors')).toEqual(availableTutorsAfter);
-
-        createSubjectComponent.unmount();
     });
 });
