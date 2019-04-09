082b413b9a671a97184628cb27bb61888beb586f
tduong992 <tduong992@gmail.com>
2019-04-09 02:12:29 +0200

    Add Create Subject page title

--- src/main/pages/CreateSubjectPage.jsx
+++ src/main/pages/CreateSubjectPage.jsx
@@ -4,10 +4,15 @@ import { withNamespaces } from 'react-i18next';
 
 
 class CreateSubjectPage extends Component {
+
     render() {
         const { t } = this.props;
+
         return (
             <React.Fragment>
+                <h1>{ t('createSubject.pageTitle') }</h1>
+                <br/>
+
                 <CreateSubject t={ t }/>
             </React.Fragment>
         );
