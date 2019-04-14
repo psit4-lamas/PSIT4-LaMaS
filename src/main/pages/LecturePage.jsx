67d5403cd2ef2b20a930b2fc1525368fb92bc7b3
tduong992 <tduong992@gmail.com>
2019-04-14 22:50:36 +0200

    Wrap LecturePage in a Form and handle its menus (left aside & sub-menu)

--- src/main/pages/LecturePage.jsx
+++ src/main/pages/LecturePage.jsx
@@ -1,52 +1,216 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { withRouterAndRedux } from '../../utils';
-import { loadSubject } from '../actions/index';
-import { isEmptyObject } from '../../utils';
+import { selectLecture, loadSubject, saveSubject } from '../actions';
+import { withRouterAndRedux, isEmptyObject } from '../../utils';
+import { Grid, Segment, Menu, Dropdown, Button, Form } from 'semantic-ui-react';
+import EditLectureBodyContent from '../LectureComponents/EditLectureBodyContent';
 import LectureBodyContent from '../LectureComponents/LectureBodyContent';
 import LoadingPage from '../pages/LoadingPage';
 import './LecturePage.css';
-import EditLectureBodyContent from '../LectureComponents/EditLectureBodyContent';
 
 
 class LecturePage extends Component {
-    // TODO: improve lecture page UI (Sprint 2)
-    componentWillMount() {
-        const { subject_id } = this.props.match.params;
-        this.props.loadSubject(subject_id);
+
+    constructor(props) {
+        super(props);
+        const lectureID = 'lecture_01';
+
+        this.state = {
+            isLoadingSubject: true,
+            subject: {},
+            lectureID: lectureID,
+            currentLecture: {},
+            isEditMode: false,
+            mode: 'view',
+            lectureName: '',
+            isValid: true,
+        };
+
+        this.refTest = React.createRef();
     }
 
+    componentWillMount() {
+        const { subject_id } = this.props.match.params;
+        const lectureID = 'lecture_01';
+
+        this.props.loadSubject(subject_id)
+            .then((response) => {
+                const currentLecture = response.subject.lectures[lectureID];
+
+                this.setState({
+                    isLoadingSubject: false,
+                    lectureID: lectureID,
+                    subject: response.subject,
+                    currentLecture: currentLecture,
+                    lectureName: currentLecture.name || '',
+                });
+            });
+    }
+
+    handleLectureMenuClick = (e) => {
+        // this.props.selectLecture(e.target.id);
+        const { subject } = this.state;
+
+        const lectureID = e.target.id;
+        const currentLecture = subject.lectures[lectureID];
+
+        this.setState({
+            lectureID: lectureID,
+            currentLecture: currentLecture,
+            lectureName: currentLecture.name || '',
+        });
+    };
+
+    handlePublishLecture = (e) => {
+        // TODO: add action to POST a request to publish this lecture
+        console.log('Not yet implemented!', e.target.value);
+    };
+
+    handleSaveLecture = () => {
+        this.props.saveSubject(this.state.subject);
+    };
+
+    onModeChange = (e, { value }) => {
+        // Force mode options to accept only: 'edit' | 'view'
+        const mode = value === 'edit' ? 'view' : 'edit';
+
+        this.setState({
+            isEditMode: mode === 'edit',
+            mode: mode,
+        });
+    };
+
+    onLectureTitleChange = (value) => {
+        const { subject, lectureID } = this.state;
+        subject.lectures[lectureID].name = value;
+
+        this.setState({
+            subject: subject,
+            lectureName: value,
+            isValid: value !== '',
+        });
+    };
+
+    renderOnViewModeDropdown = () => {
+        // TODO: add check for the current lecture is_published: true | false
+        const { subject } = this.state;
+        const { t } = this.props;
+
+        return (
+            <Dropdown id="dropdown-lecture" button className="icon" floating labeled icon="pencil"
+                      additionPosition="bottom" text={ t('menu.actions') }
+            >
+                <Dropdown.Menu>
+                    <Dropdown.Item value={ 'view' } onClick={ this.onModeChange }>
+                        { t('menu.editLecture') }
+                    </Dropdown.Item>
+                    <Dropdown.Item value={ subject.subject_id } onClick={ this.handlePublishLecture }>
+                        { t('menu.unpublish') }
+                    </Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+        );
+    };
+
+    renderOnEditModeDropdown = () => {
+        // TODO: check how to retrieve the form data to be submitted
+        const { t } = this.props;
+
+        return (
+            <Button onClick={ this.handleSaveLecture }>{ t('menu.save') }</Button>
+        );
+    };
+
+    renderActionsDropdown = () => {
+        const { mode } = this.state;
+
+        return (
+            <Segment>
+                <Menu.Menu id="top-menu-lecture" position="right">
+                    { mode === 'view'
+                      ? this.renderOnViewModeDropdown()
+                      : this.renderOnEditModeDropdown()
+                    }
+                </Menu.Menu>
+            </Segment>
+        );
+    };
+
     render() {
-        const { pathname, isLoadingSubject, subject } = this.props;
+        const { isLoadingSubject, subject, lectureID, isEditMode, isValid, lectureName } = this.state;
+
         if (isLoadingSubject || isEmptyObject(subject)) {
             return (
                 <React.Fragment>
                     <LoadingPage/>
                 </React.Fragment>
             );
-        } else {
-            return (
-                <React.Fragment>
-                    {/* TODO: add proper routes for tutor VS student view */ }
-                    <Switch>
-                        <Route path={ '/courses/:subject_id/:subject/edit' } render={ () => <EditLectureBodyContent base={ window.location.pathname }/> }/>
-                        <Route exact path={ `${ pathname }` } render={ () => <LectureBodyContent pathname={ subject['subject_name'] }/> }/>
-                    </Switch>
-                    {/*<Route exact path={ `${ this.props.base }/:subj` } render={ ({ match }) => <LectureBodyContent match={ match }/> }/>*/ }
-                </React.Fragment>
-            );
         }
+
+        let { currentLecture } = this.state;
+        const { t } = this.props;
+        const { lectures } = subject;
+        currentLecture = !!currentLecture && isEmptyObject(currentLecture) ? lectures[lectureID] : currentLecture;
+        let lectureTitle = '-' + lectureID.substring(lectureID.length - 2, lectureID.length);
+        lectureTitle = lectureTitle.replace('-0', '').replace('-', '');
+
+        return (
+            <>
+                <Form onSubmit={ this.handleSubmit }>
+
+                    { this.renderActionsDropdown() }
+
+                    <Grid columns={ 3 }>
+
+                        <Grid.Column width={ 3 }>
+                            <Menu fluid vertical tabular>
+                                { Object.keys(lectures)
+                                        .map((index, key) => (
+                                            <Menu.Item
+                                                name={ t('baseLayout.lecture') + (key + 1) }
+                                                id={ index }
+                                                key={ index }
+                                                active={ lectureID === index }
+                                                onClick={ this.handleLectureMenuClick }
+                                            />
+                                        ))
+                                }
+                            </Menu>
+                        </Grid.Column>
+
+                        <Grid.Column width={ 10 }>
+                            {/* TODO: add proper routes for tutor VS student view */ }
+                            { isEditMode && <EditLectureBodyContent
+                                t={ t }
+                                lecture={ currentLecture }
+                                lectureTitle={ lectureTitle }
+                                lectureName={ lectureName }
+                                isValid={ isValid }
+                                onLectureTitleChange={ this.onLectureTitleChange }
+                            /> }
+                            { !isEditMode && <LectureBodyContent
+                                t={ t }
+                                lecture={ currentLecture }
+                                lectureTitle={ lectureTitle }
+                            /> }
+                            {/*<Route exact path={ `${ this.props.base }/:subj` } render={ ({ match }) => <LectureBodyContent match={ match }/> }/>*/ }
+                        </Grid.Column>
+
+                    </Grid>
+                </Form>
+            </>
+        );
     }
 }
 
 
 const mapStateToProps = (state) => ( {
-    isLoadingSubject: state.subject.isLoadingSubject,
-    subject: state.subject.currentSubject,
+    user: state.user,
 } );
 
 const mapDispatchToProps = {
+    selectLecture,
     loadSubject,
+    saveSubject,
 };
 
 export { LecturePage };
