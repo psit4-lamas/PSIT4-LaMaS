be827704c1f5079c688dfce77c0d6402dfe5ba96
tduong992 <tduong992@gmail.com>
2019-04-21 16:15:36 +0200

    Make MessageBox code slimmer (KISS principle)

--- src/main/CreateSubject/MessageBox.jsx
+++ src/main/CreateSubject/MessageBox.jsx
@@ -3,35 +3,47 @@ import { Message } from 'semantic-ui-react';
 
 
 class MessageBox extends Component {
-    render() {
-        const { t, responseSubject, submittedSubject, submittedTutors } = this.props;
+
+    renderSuccessMessage = () => {
+        const { t, submittedSubject, submittedTutors } = this.props;
 
         return (
-            <div>
-                { responseSubject.isSubmitted && responseSubject.currentSubject.subject_id ? (
-                    <Message success>
-                        <Message.Header>{ t('createSubject.successMsgTitle') }</Message.Header>
-                        <p>
-                            { t('createSubject.successMsgBox1') }
-                            <strong>{ submittedSubject }</strong>
-                            { t('createSubject.successMsgBox2') }
-                            <strong>{ submittedTutors && submittedTutors.length > 0 && submittedTutors.join(', ') }</strong>
-                            { t('createSubject.successMsgBox3') }
-                        </p>
-                    </Message>
-                ) : null }
-                { responseSubject.isSubmitted && !responseSubject.currentSubject.subject_id ? (
-                    <Message negative>
-                        <Message.Header>{ t('createSubject.negativeMsgTitle') }</Message.Header>
-                        <p>
-                            { t('createSubject.negativeMsgBox1') }
-                            <strong>{ submittedSubject }</strong>
-                            { t('createSubject.negativeMsgBox2') }
-                            <strong>{ submittedTutors && submittedTutors.length > 0 && submittedTutors.join(', ') }</strong>
-                            { t('createSubject.negativeMsgBox3') }
-                        </p>
-                    </Message>
-                ) : null }
+            <Message success>
+                <Message.Header>{ t('createSubject.successMsgTitle') }</Message.Header>
+                <p>
+                    { t('createSubject.successMsgBox1') }
+                    <strong>{ submittedSubject }</strong>
+                    { t('createSubject.successMsgBox2') }
+                    <strong>{ submittedTutors && submittedTutors.length > 0 && submittedTutors.join(', ') }</strong>
+                    { t('createSubject.successMsgBox3') }
+                </p>
+            </Message>
+        );
+    };
+
+    renderErrorMessage = () => {
+        const { t, submittedSubject, submittedTutors } = this.props;
+
+        return (
+            <Message negative>
+                <Message.Header>{ t('createSubject.negativeMsgTitle') }</Message.Header>
+                <p>
+                    { t('createSubject.negativeMsgBox1') }
+                    <strong>{ submittedSubject }</strong>
+                    { t('createSubject.negativeMsgBox2') }
+                    <strong>{ submittedTutors && submittedTutors.length > 0 && submittedTutors.join(', ') }</strong>
+                    { t('createSubject.negativeMsgBox3') }
+                </p>
+            </Message>
+        );
+    };
+
+    render() {
+        const { responseSubject } = this.props;
+
+        return (
+            <div style={ { marginBottom: '25px' } }>
+                { responseSubject.currentSubject.subject_id ? this.renderSuccessMessage() : this.renderErrorMessage() }
             </div>
         );
     }
