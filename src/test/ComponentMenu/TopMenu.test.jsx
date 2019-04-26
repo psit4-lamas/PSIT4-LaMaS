ca1f1e1ae2a44eeaa901df25a646e4e47f50a08e
Nesschen <nessi1612@gmx.de>
2019-04-26 03:29:50 +0200

    TopMenu: removed console logs

--- src/test/ComponentMenu/TopMenu.test.jsx
+++ src/test/ComponentMenu/TopMenu.test.jsx
@@ -41,13 +41,9 @@ describe('Top menu', () => {
 
     it('renders without crashing', () => {
         const intern = i18n.default;
-        console.log(intern.language);
-        console.log(i18n.languages);
 
         const menu = <TopMenu { ...props } />;
         ReactDOM.render(menu, div);
-
-        console.log(i18n.language);
 
         ReactDOM.unmountComponentAtNode(div);
     });
