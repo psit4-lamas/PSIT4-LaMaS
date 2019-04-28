b9e2eaeac36527ad38c5886e350153d7bb312194
Nesschen <nessi1612@gmx.de>
2019-04-28 23:26:52 +0200

    TopMenu: finally solved the test 'handleOnClick correctly'

--- src/test/ComponentMenu/TopMenu.test.jsx
+++ src/test/ComponentMenu/TopMenu.test.jsx
@@ -4,6 +4,7 @@ import i18n from '../../i18n';
 import { create } from 'react-test-renderer';
 import { Dropdown, Menu } from 'semantic-ui-react';
 import { TopMenu } from '../../main/ComponentMenu/TopMenu';
+import {shallow, mount} from "enzyme/build/index";
 
 describe('Top menu', () => {
     let div;
@@ -11,33 +12,57 @@ describe('Top menu', () => {
     let component;
     let rootInstance;
 
-    beforeEach(() => {
-        div = document.createElement('div');
-        props = {
-            t: (key) => key,
-            changeLanguage: (lang) => lang,
-            tabs: {
-                isLoadingTabs: false,
-                activeTabs: [ 'IS', 'KI2', 'PSIT4' ],
-                subjectLinks: [
-                    {
-                        name: 'IS',
-                        subject_id: '01234',
-                    }, {
-                        name: 'KI2',
-                        subject_id: '12345',
-                    }, {
-                        name: 'PSIT4',
-                        subject_id: '23456',
-                    },
-                ],
-            },
-        };
+    // beforeEach(() => {
+    //     div = document.createElement('div');
+    //     props = {
+    //         t: (key) => key,
+    //         changeLanguage: (lang) => lang,
+    //         tabs: {
+    //             isLoadingTabs: false,
+    //             activeTabs: [ 'IS', 'KI2', 'PSIT4' ],
+    //
+    //             subjectLinks: [
+    //                 {
+    //                     name: 'IS',
+    //                     subject_id: '01234',
+    //                 }, {
+    //                     name: 'KI2',
+    //                     subject_id: '12345',
+    //                 }, {
+    //                     name: 'PSIT4',
+    //                     subject_id: '23456',
+    //                 },
+    //             ],
+    //         },
+    //     };
+    //
+    //
+    //     component = create(<TopMenu { ...props } />);
+    //     rootInstance = component.root;
+    // });
 
+    props = {
+        t: (key) => key,
+        loadSubject: jest.fn(),
+        changeLanguage: (lang) => lang,
+        tabs: {
+            isLoadingTabs: false,
+            activeTabs: [ 'IS', 'KI2', 'PSIT4' ],
 
-        component = create(<TopMenu { ...props } />);
-        rootInstance = component.root;
-    });
+            subjectLinks: [
+                {
+                    name: 'IS',
+                    subject_id: '01234',
+                }, {
+                    name: 'KI2',
+                    subject_id: '12345',
+                }, {
+                    name: 'PSIT4',
+                    subject_id: '23456',
+                },
+            ],
+        },
+    };
 
     it('renders without crashing', () => {
         const intern = i18n.default;
@@ -48,23 +73,20 @@ describe('Top menu', () => {
         ReactDOM.unmountComponentAtNode(div);
     });
 
-    // it('handleOnClick correctly', () => {
-    //     let TopMenu, TestUtils, FluxxorTestUtils, FluxConstructor, realFlux, fakeFlux;
-    //
-    //     TestUtils = require('react-dom/test-utils');
-    //     FluxxorTestUtils = require('fluxxor-test-utils');
-    //     FluxConstructor = require('../../../app/FluxConstructor.js');
-    //     realFlux = FluxConstructor();
-    //     fakeFlux = FluxxorTestUtils.fakeFlux(realFlux);
-    //     fakeFlux.genMocksForStoresAndActions();
-    //     // now all stores and action methods are mocked for testing
-    //
-    //     TopMenu = TestUtils.renderIntoDocument(<TopMenu flux={fakeFlux} />);
-    //
-    //     TopMenu.handleItemClick = jest.genMockFunction();
-    //     TestUtils.Simulate.click(TopMenu.refs.MenuItem.getDOMNode());
-    //     expect(TopMenu.handleItemClick).toBeCalled();
-    // })
+    it('handleOnClick correctly', () => {
+        const handleItemClick = jest.fn();
+        const loadSubject = jest.fn();
+        const historyMock = { push: jest.fn() };
+
+        const component = <TopMenu { ...props } handleItemClick={ handleItemClick } history={ historyMock } loadSubject={ loadSubject } />;
+
+        const clickTopMenu = mount(component);
+
+        clickTopMenu.find('a.item').at(2).simulate('click');
+        expect(historyMock.push).toHaveBeenCalledWith('/courses/01234/IS');
+
+        // expect(handleItemClick).toHaveBeenCalled();
+    })
 
     it('should change language when clicking on Dropdown', () => {
         const dropdownItems = rootInstance.findAllByType(Dropdown.Item);
