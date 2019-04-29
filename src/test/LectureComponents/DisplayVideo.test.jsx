82daa4a89c55327e60956364a66184571d038883
waedenma <waedenma@students.zhaw.ch>
2019-04-29 14:07:15 +0200

    DisplayVideo Test added

--- src/test/LectureComponents/DisplayVideo.test.jsx
+++ src/test/LectureComponents/DisplayVideo.test.jsx
@@ -1,43 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import i18n from '../../i18n';
-import { create } from 'react-test-renderer';
+import { shallow } from 'enzyme/build';
 import { DisplayVideo } from '../../main/LectureComponents/DisplayVideo';
 
 describe('DisplayVideo', () => {
-    let div;
-    let props;
-    let component;
-    let rootInstance;
+    const videoUrl =
+        'https://firebasestorage.googleapis.com/v0/b/fir-and-react-55a5a.appspot.com/o/files%2F9786e06c-3be2-4090-80f2-6fc28d2eb2dd.mp4?alt=media&token=70d24a4c-a2d0-4d1f-bb22-07913c61f845';
+    const nameOnStorage = 'files/9786e06c-3be2-4090-80f2-6fc28d2eb2dd.mp4';
 
-    it ('nothing', () => {
+    describe('DisplayVideo', () => {
+        describe('should match Snapshot', () => {
+            it('with VideoUrl', () => {
+                const component = shallow(<DisplayVideo t={ (key) => key } videoUrl={ videoUrl } nameOnStorage={ nameOnStorage }/>);
 
+                expect(component).toMatchSnapshot();
+
+                component.unmount();
+            });
+
+            it('without VideoUrl', () => {
+                const component = shallow(<DisplayVideo t={ (key) => key } nameOnStorage={ nameOnStorage }/>);
+
+                expect(component).toMatchSnapshot();
+
+                component.unmount();
+            });
+        });
     });
-
-    // beforeEach(() => {
-    //     div = document.createElement('div');
-    //     props = {
-    //         t: (key) => key,
-    //         fetchedVideo: '',
-    //         nameOnStorage: 'files/9786e06c-3be2-4090-80f2-6fc28d2eb2dd.mp4',
-    //         videoUrl: 'https://firebasestorage.googleapis.com/v0/b/fir-and-react-55a5a.appspot.com/o/files%2F9786e06c-3be2-4090-80f2-6fc28d2eb2dd.mp4?alt=media&token=70d24a4c-a2d0-4d1f-bb22-07913c61f845'
-    //     };
-    //
-    //
-    //     component = create(<DisplayVideo {...props} />);
-    //     rootInstance = component.root;
-    // });
-    //
-    // it('renders without crashing', () => {
-    //     const intern = i18n.default;
-    //     console.log(intern.language);
-    //     console.log(i18n.languages);
-    //
-    //     const menu = <DisplayVideo {...props} />;
-    //     ReactDOM.render(menu, div);
-    //
-    //     console.log(i18n.language);
-    //
-    //     ReactDOM.unmountComponentAtNode(div);
-    // });
 });
