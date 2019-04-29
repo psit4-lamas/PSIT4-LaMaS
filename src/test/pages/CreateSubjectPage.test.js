0897b7eba3bb53999049ae6aad081691bfd0cf32
waedenma <waedenma@students.zhaw.ch>
2019-04-29 14:21:49 +0200

    CreateSubjectPage coverage increased

--- src/test/pages/CreateSubjectPage.test.js
+++ src/test/pages/CreateSubjectPage.test.js
@@ -2,40 +2,42 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import CreateSubjectPage from '../../main/pages/CreateSubjectPage';
 
-
 describe('CreateSubjectPage', () => {
     let renderedComponent;
 
-    // const user = Object.freeze({
-    //     isLoadingUser: true,
-    // });
+    const propsIsStudent = {
+        t: jest.fn(),
+        user: {
+            isStudent: true,
+        },
+    };
 
-    const props = {
+    const propsIsNotStudent = {
         t: jest.fn(),
         user: {
             isStudent: false,
         },
     };
 
-    // it('renders without crashing', () => {
-    //     const div = document.createElement('div');
-    //
-    //     ReactDOM.render(<BaseLayout t={ (key) => key } user={ user } />, div);
-    //     ReactDOM.unmountComponentAtNode(div);
-    // });
-
-    beforeEach(() => {
-
-        const component = <CreateSubjectPage { ...props } />;
-
-        renderedComponent = shallow(component);
-    });
-
     afterEach(() => {
         renderedComponent.unmount();
     });
 
-    it('should match snapshot', () => {
-        expect(renderedComponent).toMatchSnapshot();
+    describe('should match snapshot', () => {
+        it('if user.isStudent', () => {
+            const component = <CreateSubjectPage { ...propsIsStudent } />;
+
+            renderedComponent = shallow(component);
+
+            expect(renderedComponent).toMatchSnapshot();
+        });
+
+        it('if not', () => {
+            const component = <CreateSubjectPage { ...propsIsNotStudent } />;
+
+            renderedComponent = shallow(component);
+
+            expect(renderedComponent).toMatchSnapshot();
+        });
     });
 });
