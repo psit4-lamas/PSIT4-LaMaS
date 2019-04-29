ae6f43fa0fefb376500e2e0b830f17816d280040
tduong992 <tduong992@gmail.com>
2019-04-29 17:58:03 +0200

    Move out DisplayVideo tests from nested test suit

--- src/test/LectureComponents/DisplayVideo.test.jsx
+++ src/test/LectureComponents/DisplayVideo.test.jsx
@@ -2,28 +2,25 @@ import React from 'react';
 import { shallow } from 'enzyme/build';
 import { DisplayVideo } from '../../main/LectureComponents/DisplayVideo';
 
+
 describe('DisplayVideo', () => {
     const videoUrl =
         'https://firebasestorage.googleapis.com/v0/b/fir-and-react-55a5a.appspot.com/o/files%2F9786e06c-3be2-4090-80f2-6fc28d2eb2dd.mp4?alt=media&token=70d24a4c-a2d0-4d1f-bb22-07913c61f845';
     const nameOnStorage = 'files/9786e06c-3be2-4090-80f2-6fc28d2eb2dd.mp4';
 
-    describe('DisplayVideo', () => {
-        describe('should match Snapshot', () => {
-            it('with VideoUrl', () => {
-                const component = shallow(<DisplayVideo t={ (key) => key } videoUrl={ videoUrl } nameOnStorage={ nameOnStorage }/>);
+    it('should match Snapshot with VideoUrl', () => {
+        const component = shallow(<DisplayVideo t={ (key) => key } videoUrl={ videoUrl } nameOnStorage={ nameOnStorage }/>);
 
-                expect(component).toMatchSnapshot();
+        expect(component).toMatchSnapshot();
 
-                component.unmount();
-            });
+        component.unmount();
+    });
 
-            it('without VideoUrl', () => {
-                const component = shallow(<DisplayVideo t={ (key) => key } nameOnStorage={ nameOnStorage }/>);
+    it('should match Snapshot without VideoUrl', () => {
+        const component = shallow(<DisplayVideo t={ (key) => key } nameOnStorage={ nameOnStorage }/>);
 
-                expect(component).toMatchSnapshot();
+        expect(component).toMatchSnapshot();
 
-                component.unmount();
-            });
-        });
+        component.unmount();
     });
 });
