c6b9c52031b3286d51c76b4cbe7d15470fd6dc6c
kernda02 <kernda02@students.zhaw.ch>
2019-04-29 12:27:39 +0200

    added E2E test for click on lecture

--- src/test/acceptance/SelectLectureTest.js
+++ src/test/acceptance/SelectLectureTest.js
@@ -9,36 +9,18 @@ module.exports = {
             .setValue('input[type=text]', 'dummy1@students.zhaw.ch')
             .setValue('input[name=password]', 'dummy1')
             .click('button[type=submit]')
-            .waitForElementVisible('#top-menu-logout',10000)
-            .end();
+            .waitForElementVisible('#top-menu-logout',10000);
     },
 
-    'Step Login with invalid credentials' : function (browser) {
+    'Click on Lecture' : function (browser) {
         browser
             .url('http://localhost:3000')
-            .waitForElementVisible('input[name=email]',10000)
-            .setValue('input[type=text]', 'someone@somedomain.com')
-            .setValue('input[name=password]', 'someone')
-            .click('button[type=submit]')
+            .waitForElementVisible('div[id="top-menu"]',10000)
             .pause(10000)
-            // TODO: define Login Error Flow
-            // assume Login was correct if topMenu Logout Button is not present
-            .verify.elementNotPresent('#top-menu-logout')
+            .useXpath()     // every selector now must be XPath
+            .click("//a[text()='PSIT3']")
+            .verify.elementPresent("//main/form/div/div/div/div/a[text()='PSIT3']")
             .end();
     },
 
-    'Logout' : function (browser) {
-        browser
-            .url('http://localhost:3000')
-            .waitForElementVisible('input[name=email]',10000)
-            .setValue('input[type=text]', 'dummy1@students.zhaw.ch')
-            .setValue('input[name=password]', 'dummy1')
-            .click('button[type=submit]')
-            // assume Login was correct if topMenu Logout Button is visible
-            .waitForElementVisible('#top-menu-logout',10000)
-            .click('button#logout')
-            // check if LoginPage becomes visible after Logout
-            .waitForElementVisible('button[type=submit]',10000)
-            .end();
-    }
 };
