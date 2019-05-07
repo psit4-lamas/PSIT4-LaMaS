c43a5457fbcf00801872a016c4a59fae5e6c4609
wueesmar <wueesmar@students.zhaw.ch>
2019-05-08 00:44:42 +0200

    refactor of component

--- src/test/Comment.test.js
+++ src/test/Comment.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Comment from '../main/Comment/Comment';
+import CommentAdd from '../main/Comment/CommentAdd';
 import { mount } from 'enzyme';
 
 
 it('checks if "saveMessage" has been called', () => {
     const saveMessage = jest.fn();
-    const comment = mount(<Comment saveMessage={ saveMessage }/>);
+    const comment = mount(<CommentAdd saveMessage={ saveMessage }/>);
 
     comment.find('form').simulate('submit');
     expect(saveMessage).toHaveBeenCalled();
@@ -14,7 +14,7 @@ it('checks if "saveMessage" has been called', () => {
 
 it('should set components state value after changed input text', function() {
     const event = { target: { value: 'test' } };
-    const comment = mount(<Comment/>);
+    const comment = mount(<CommentAdd/>);
 
     comment.find('input').simulate('change', event);
     expect(comment.state('value')).toEqual(event.target.value);
